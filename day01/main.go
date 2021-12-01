66ab822738fe2111b72f394812caaea75b20eb64
MattiasBerlin <mattias-berlin@hotmail.com>
2021-12-01 07:39:31 +0100

    Change some var names

--- day01/main.go
+++ day01/main.go
@@ -10,39 +10,39 @@ import (
 func main() {
 	splitInput := utils.SplitInput("day01/input.txt")
 
-	increased := increaseCount(splitInput)
-	fmt.Printf("Task 1. There were %d measurements that increased\n", increased)
+	increaseCount := increaseCount(splitInput)
+	fmt.Printf("Task 1. There were %d measurements that increased\n", increaseCount)
 	threeSumIncreaseCount := threeSumIncreaseCount(splitInput)
 	fmt.Printf("Task 2. There were %d three-sum measurements that increased\n", threeSumIncreaseCount)
 }
 
 func increaseCount(s []string) int {
-	increased := 0
+	count := 0
 	lastVal, _ := strconv.Atoi(s[0])
 	for _, line := range s[1:] {
 		curVal, _ := strconv.Atoi(line)
 		if curVal > lastVal {
-			increased++
+			count++
 		}
 		lastVal = curVal
 	}
-	return increased
+	return count
 }
 
 func threeSumIncreaseCount(s []string) int {
-	increased := 0
+	count := 0
 	val1, _ := strconv.Atoi(s[0])
 	val2, _ := strconv.Atoi(s[1])
-	lastVal := 99999999
+	lastSum := 99999999
 	for _, line := range s[2:] {
 		val3, _ := strconv.Atoi(line)
 		sum := val1 + val2 + val3
-		if sum > lastVal {
-			increased++
+		if sum > lastSum {
+			count++
 		}
 		val1 = val2
 		val2 = val3
-		lastVal = sum
+		lastSum = sum
 	}
-	return increased
+	return count
 }
