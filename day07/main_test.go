2769111b84cf374b809b8b71b3522a8cb7290209
MattiasBerlin <mattias-berlin@hotmail.com>
2021-12-07 21:21:58 +0100

    Fix test error message

--- day07/main_test.go
+++ day07/main_test.go
@@ -12,7 +12,7 @@ func Test_findCheapestPosition(t *testing.T) {
 	positions := parse(input)
 	res := findCheapestPosition(positions, linearCost)
 	if res != 37 {
-		t.Errorf("Should return 2, not %d", res)
+		t.Errorf("Should return 37, not %d", res)
 	}
 
 	res = findCheapestPosition(positions, exponentialCost)
