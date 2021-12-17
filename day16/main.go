ac41f6e10af0cf164ebce4249d1fed01bb78653a
MattiasBerlin <mattias-berlin@hotmail.com>
2021-12-17 20:50:56 +0100

    Add day 16 solution

--- day16/main.go
+++ day16/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
+
+	"github.com/dotMaro/AoC2021/utils"
 )
 
 func main() {
-	const test = "8A004A801A8002F478"
-	binary := convertHexToBinary(test)
-	fmt.Println(binary)
-	fmt.Println(versionSum(binary))
+	input := utils.InputString("day16/input.txt")
+	binary := convertHexToBinary(input)
+	pkg, _, versionSum := parseNextPackage(binary)
+	fmt.Printf("Task 1. The sum of all versions is %d\n", versionSum)
+	fmt.Printf("Task 2. The evaluated expression is %d\n", pkg.resolve())
 }
 
 func convertHexToBinary(s string) string {
@@ -39,35 +43,19 @@ func convertHexToBinary(s string) string {
 	return binary.String()
 }
 
-func versionSum(s string) int {
-	versionSum := 0
-
-	offsetSum := 0
-	lastOffset := 0
-	for offsetSum < len(s)-3 {
-		_, offset, sum := parseNextPackage(s[lastOffset:])
-		// fmt.Printf("%d - %d\n", offset, len(s))
-		offsetSum += offset
-		versionSum += sum
-		// offset2 := offset
-		// if offset2%4 != 0 {
-		// 	offset2 += 4 - offset2%4
-		// }
-		lastOffset = offset
-	}
-	return versionSum
-}
-
 func parseNextPackage(s string) (bitsPackage, int, int) {
 	version, _ := strconv.ParseUint(s[0:3], 2, 8)
 	typeID, _ := strconv.ParseUint(s[3:6], 2, 8)
 	var endOffset int
+	value := -1
+	var subpackages []bitsPackage
 
 	versionSum := int(version)
 
 	switch typeID {
 	case 4: // Literal value.
 		reachedEnd := false
+		var valueStr strings.Builder
 		for start := 6; !reachedEnd; start += 5 {
 			end := start + 5
 			group := s[start:end]
@@ -75,16 +63,18 @@ func parseNextPackage(s string) (bitsPackage, int, int) {
 				reachedEnd = true
 			}
 			endOffset = end
-			// For now, ignore the value.
+			valueStr.WriteString(group[1:5])
 		}
+		val, _ := strconv.ParseUint(valueStr.String(), 2, 64)
+		value = int(val)
 	default: // Operator.
 		subpackageOffset := 0
 		switch s[6] {
 		case '0': // Length type.
 			length, _ := strconv.ParseUint(s[7:22], 2, 16)
-			// subpackageOffsets := 0
 			for subpackageOffset < int(length) {
-				_, offset, sum := parseNextPackage(s[22+subpackageOffset:])
+				pkg, offset, sum := parseNextPackage(s[22+subpackageOffset:])
+				subpackages = append(subpackages, pkg)
 				versionSum += sum
 				subpackageOffset += offset
 			}
@@ -93,28 +83,81 @@ func parseNextPackage(s string) (bitsPackage, int, int) {
 			numberOfSubpackets, _ := strconv.ParseUint(s[7:18], 2, 16)
 			count := 0
 			for count < int(numberOfSubpackets) {
-				_, offset, sum := parseNextPackage(s[18+subpackageOffset:])
+				pkg, offset, sum := parseNextPackage(s[18+subpackageOffset:])
+				subpackages = append(subpackages, pkg)
 				versionSum += sum
 				subpackageOffset += offset
 				count++
 			}
 			endOffset = 18 + subpackageOffset
 		}
-		// if endOffset%4 != 0 {
-		// 	endOffset += 4 - endOffset%4
-		// }
 	}
 
-	// if endOffset%4 != 0 {
-	// 	endOffset += 4 - endOffset%4
-	// }
 	return bitsPackage{
-		version: int(version),
-		typeID:  int(typeID),
+		version:     int(version),
+		typeID:      int(typeID),
+		value:       value,
+		subpackages: subpackages,
 	}, endOffset, versionSum
 }
 
 type bitsPackage struct {
-	version int
-	typeID  int
+	version     int
+	typeID      int
+	value       int
+	subpackages []bitsPackage
+}
+
+func (p bitsPackage) resolve() int {
+	switch p.typeID {
+	case 0: // Sum.
+		sum := 0
+		for _, s := range p.subpackages {
+			sum += s.resolve()
+		}
+		return sum
+	case 1: // Product.
+		product := 1
+		for _, s := range p.subpackages {
+			product *= s.resolve()
+		}
+		return product
+	case 2: // Minimum.
+		minimum := math.MaxInt32
+		for _, s := range p.subpackages {
+			val := s.resolve()
+			if val < minimum {
+				minimum = val
+			}
+		}
+		return minimum
+	case 3: // Maximum.
+		maximum := 0
+		for _, s := range p.subpackages {
+			val := s.resolve()
+			if val > maximum {
+				maximum = val
+			}
+		}
+		return maximum
+	case 4: // Value (not an operator).
+		return p.value
+	case 5: // Greater than.
+		if p.subpackages[0].resolve() > p.subpackages[1].resolve() {
+			return 1
+		}
+		return 0
+	case 6: // Less than.
+		if p.subpackages[0].resolve() < p.subpackages[1].resolve() {
+			return 1
+		}
+		return 0
+	case 7: // Equal to.
+		if p.subpackages[0].resolve() == p.subpackages[1].resolve() {
+			return 1
+		}
+		return 0
+	default:
+		panic(fmt.Sprintf("Bad operation typeID %d", p.typeID))
+	}
 }
--- day16/main_test.go
+++ day16/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func Test_versionSum(t *testing.T) {
+func Test_parseNextPackage_versionSum(t *testing.T) {
 	testCases := []struct {
 		input string
 		exp   int
@@ -10,14 +10,39 @@ func Test_versionSum(t *testing.T) {
 		{input: "8A004A801A8002F478", exp: 16},
 		{input: "620080001611562C8802118E34", exp: 12},
 		{input: "C0015000016115A2E0802F182340", exp: 23},
+		{input: "A0016C880162017C3686B18A3D4780", exp: 31},
 	}
 
 	for _, tc := range testCases {
 		binary := convertHexToBinary(tc.input)
-		t.Log("\n", binary)
-		res := versionSum(binary)
+		_, _, res := parseNextPackage(binary)
 		if res != tc.exp {
-			t.Errorf("Should return %d, not %d", tc.exp, res)
+			t.Errorf("Should return %d, not %d, for input %s", tc.exp, res, tc.input)
+		}
+	}
+}
+
+func Test_bitPackage_resolve(t *testing.T) {
+	testCases := []struct {
+		input string
+		exp   int
+	}{
+		{input: "C200B40A82", exp: 3},
+		{input: "04005AC33890", exp: 54},
+		{input: "880086C3E88112", exp: 7},
+		{input: "CE00C43D881120", exp: 9},
+		{input: "D8005AC2A8F0", exp: 1},
+		{input: "F600BC2D8F", exp: 0},
+		{input: "9C005AC2F8F0", exp: 0},
+		{input: "9C0141080250320F1802104A08", exp: 1},
+	}
+
+	for _, tc := range testCases {
+		binary := convertHexToBinary(tc.input)
+		pkg, _, _ := parseNextPackage(binary)
+		res := pkg.resolve()
+		if res != tc.exp {
+			t.Errorf("Should return %d, not %d, for input %s", tc.exp, res, tc.input)
 		}
 	}
 }
